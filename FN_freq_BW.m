a7c83f06e6990d92190e9ddf868075fecdc3321b
matthewdyeung <yeungmat@usc.edu>
2023-07-19 10:02:51 -0400

    Commented matlab files

--- FN_freq_BW.m
+++ FN_freq_BW.m
@@ -1,18 +1,21 @@
-yc=1.69;
+yc=1.69;%center wavlength in micron
 c=2.99792458e8 ;
 wc=2*pi*c*1e-15./(yc*1e-6);
-t=linspace(-100,100,50000);
-cycle_count = yc * (1e-6) / (c * 1e-15);
-fwhm=cycle_count*10;
-phi_ce=0;
+t=linspace(-100,100,50000); %time axis
+cycle_count = yc * (1e-6) / (c * 1e-15); 
+fwhm=cycle_count*10; %cycle count of pulse
+phi_ce=0; %carrier envelope phase of pulse
 GDD=0;
 TOD=0;
 FOD=0;
+workfunction=5.1; %workfunction of gold
 pulse=gaussianPulseHOD(t,fwhm,wc,phi_ce,GDD,TOD,FOD);
-FN=J_FN_SI_Asym(pulse*32,5.1);
+FNBW=dJ_FN_SI_Asym(pulse*32,workfunction); %derivative of the FN to get the taylor expanded frequecny bandwidth
 
-plot(t,FN)
-[f,Af]=fftout(t,FN);
+%use J_FN_SI_Asym(pulse,workfunction) for getting time domain electron
+%emission rate
+
+[f,Af]=fftout(t,FNBW);
 nAf=Af./max(Af)';
 f=f';
 plot(f,nAf) 
--- dJ_FN_SI_Asym.m
+++ dJ_FN_SI_Asym.m
@@ -1,13 +1,13 @@
 function dJ=dJ_FN_SI_Asym(F, phi)
 dJ=zeros(1,length(F));
-%F = -1*F;
+%F = -1*F;  %we defined field in a certain direction, so we use F=F
 for i=1:length(F)
     if F(i)>0
         a_const = 1.541534e-6;
         b_const = 6.83089;
 
         f = 1.43996453529595*F(i)/phi.^2;
-        v_f = 1;%1 - f + 1/6.*f.*log(abs(f));
+        v_f = 1;%1 - f + 1/6.*f.*log(abs(f));%simplified FN v_f=1
         alpha = a_const/phi;
         beta = v_f*b_const*phi.^(3/2);
         dJ(i) = (2*a_const/phi*F(i) + a_const*v_f*b_const*phi.^(1/2))*exp(-v_f*b_const*phi.^(3/2)/F(i));
